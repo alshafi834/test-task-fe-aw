971d3789cd27e2328867af55cf94bcd71584f7eb
alshafi834 <alshafi834@gmail.com>
2022-11-20 14:20:15 +0100

    separated the post card component

--- src/components/Posts/LatestPosts.vue
+++ src/components/Posts/LatestPosts.vue
@@ -5,14 +5,7 @@
     <div class="container">
       <div class="row gx-5 gy-5">
         <div class="col" v-for="post in latestPosts" :key="post">
-          <div class="card h-100" style="width: 15rem">
-            <div class="card-body">
-              <h5 class="card-title">{{ post.title.substr(0, 20) }}</h5>
-              <p class="card-text">
-                {{ post.body.substr(0, 100) }}
-              </p>
-            </div>
-          </div>
+          <post-card :post="post" />
         </div>
       </div>
     </div>
@@ -22,27 +15,21 @@
 <script lang="ts">
 import { onMounted, computed, defineComponent } from "vue";
 import { useStore } from "vuex";
-//import axios from "axios";
 import { FETCH_POSTS } from "../../store/constants";
 import { key } from "../../store";
 import { Post } from "../../api/types";
+import PostCard from "./PostCard.vue";
 
 export default defineComponent({
   name: "LatestPosts",
+  components: { PostCard },
   setup() {
     const store = useStore(key);
-    //const latestPosts = store.state.posts;
     const latestPosts = computed((): Post[] => store.state.posts);
 
     const fetchPosts = () => {
       store.dispatch(FETCH_POSTS);
     };
-
-    /* const fetchmyPost = async () => {
-      const baseUrl = "https://gorest.co.in/public/v2";
-      const response = await axios.get(`${baseUrl}/posts`);
-      latestPosts.value = response.data;
-    }; */
 
     onMounted(fetchPosts);
 
---
+++ src/components/Posts/PostCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card h-100" style="width: 15rem">
+    <div class="card-body">
+      <h5 class="card-title">{{ post.title.substr(0, 20) }}</h5>
+      <p class="card-text">
+        {{ post.body.substr(0, 100) }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "PostCard",
+  props: {
+    post: {
+      userId: Number,
+      id: Number,
+      title: String,
+      body: String,
+    },
+  },
+});
+</script>
